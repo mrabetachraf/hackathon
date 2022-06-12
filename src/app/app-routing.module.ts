4b99274bc7cf056fbb8637f23906297a6ff89299
mohamedachraf mrabet <mohamedahraf.mrabet@esprit.tn>
2022-06-12 03:30:41 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+
+import { CheckoutComponent } from './checkout/checkout.component';
+
+import { ShopComponent } from './shop/shop.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"product-single", component:ProductsingleComponent },
+  { path:"cart", component:CartComponent },
+  { path:"checkout", component:CheckoutComponent },
+  { path:"shop", component:ShopComponent },
+  { path:"dashboard", component:DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsingleComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
